fa86695ffb60f7580ec9e60bf035e00e1273aade
Bruno Amorim <0brunoamorim0@gmail.com>
2023-10-15 19:58:48 -0300

    Commit Inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactInfoComponent } from './components/contact-info/contact-info.component';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ContentComponent } from './pages/content/content.component'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactInfoComponent,
+    MenuTitleComponent,
+    BigCardComponent,
+    SmallCardComponent,
+    HomeComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/data/fakeData.ts
@@ -0,0 +1,25 @@
+export const dataFake = [
+  {"id":"1",
+    "title": "Novo jogo de tabuleiro",
+    "description": "Novo jogo de tabuleiro será lançado em breve! Acesse para saber mais.",
+    "photoCover": "https://img.olhardigital.com.br/wp-content/uploads/2021/02/Stardew-Valley.png",
+    "cardDescription":"Novo jogo de tabuleiro será lançado em breve! Acesse para saber mais."
+  },
+  {"id":"2",
+    "title": "Melhorando a sua plantação: Lavoura",
+    "description": "As Lavouras são plantas que crescem de sementes para serem colhidas com o proposito de gerar lucro, alimento ou presentes. Geralmente cada lavoura varia por estação. Quando as estações mudam (após dia 28) os cultivos irão murchar e morrer (com exceção do Grão de café, Girassol, Milho, Trigo e Fruta antiga). Lavouras incluem frutas e vegetais que podem ser processados em Geleias e picles através da Jarra de conserva ou Vinho e Suco através do Barril. Os cultivos que precisam ser enviados para a conquista Policultura não incluem Flores, Frutas de Árvores frutíferas, itens de coleta (com exceção de Uvas), Fruta Antiga ou Cereja de Joia Doce. Entretanto após a versão 1.1, o jogador também precisará enviar 15 sementes de café para desbloquear a conquista.",
+    "photoCover": "https://www.stardewvalley.net/wp-content/uploads/2018/12/1_1screenshot15.png",
+  },
+  {"id":"3",
+    "title": "Comportamento dos peixes",
+    "description": "Comportamento. Existem 5 tipos de peixes no mini-jogo: Misto: usam o padrão de movimento básico. Suave: têm um padrão de movimento mais estável. Pesado: têm uma aceleração mais rápida para baixo. Flutuador: têm uma aceleração mais rápida para cima. Dardo: movem-se com uma amplitude de movimento percentualmente duas vezes maior em ambas as direções",
+    "photoCover": "https://cdn.ome.lt/NF9bx8G9pbXgC42uaIfi9kcn2yM=/1200x630/smart/extras/conteudos/stardew_valley_praia.png",
+  },
+  {"id":"4",
+    "title": "Novidades para os pets",
+    "description": "Na atualização 1.6 os pets, tanto gatos como cachorros, poderão usar chapéus!",
+    "photoCover": "https://pbs.twimg.com/media/F7s3PTMa4AAvNaL?format=png&name=360x360",
+  }
+
+
+]
